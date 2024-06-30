fa7cb3395faf4b395dc5464e5683552378d674d6
BMISYTEM <baironmenesidarraga.990128@gmail.com>
2024-06-30 13:45:59 -0500

    cambio de stylo

--- src/Home/Header/LayoutHeader.tsx
+++ src/Home/Header/LayoutHeader.tsx
@@ -2,7 +2,7 @@ import { Navar } from './Components/Navar';
 import { Header } from './Components/Header';
 export const LayoutHeader = () => {
   return (
-    <section className="w-full flex flex-col ">
+    <section className="w-full h-full flex flex-col ">
         {/* barra de navegacion */}
         <Navar/>
         {/* imagen header o banner  */}
