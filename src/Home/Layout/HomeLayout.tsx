9beb5f365082f1ff76bca8d6c3a0d923d09c4058
BMISYTEM <baironmenesidarraga.990128@gmail.com>
2024-06-30 13:50:17 -0500

    se agrego el boton de wpp y de waze

--- src/Home/Layout/HomeLayout.tsx
+++ src/Home/Layout/HomeLayout.tsx
@@ -11,16 +11,28 @@ import { HomeVision } from "../Vision/Layout/HomeVision"
 import { HomeProviderContext } from "../Context/HomeContext"
 import { HomeModales } from "../Modales/HomeModales"
 import { ToastContainer } from "react-toastify"
+import { Link } from "react-router-dom"
 
 export const HomeLayout = () => {
   return (
     <HomeProviderContext>
-
+      <div className="">
+          <div className='fixed z-50 w-16 h-16 bottom-[5%] md:left-[92%] left-[80%] hover:scale-105 transition-all'>
+              <Link to={'https://wa.me/message/IH5SZDTONQ5MK1'}>
+                <img src={'/Imagenes/whatsapp.png'} alt="" className=' h-auto hover:cursor-pointer hover:animate-pulse' />
+              </Link>
+          </div>
+          <div className='fixed z-50 w-16 bg-sky-800 flex justify-center rounded-full  items-center h-16 bottom-[15%] md:left-[92%] left-[80%] hover:scale-105 transition-all '>
+              <Link to={'https://waze.com/ul?ll=3.397535343008182, -76.54328168393025&navigate=yes'} className="">
+                <img src={'/Imagenes/waze.png'} alt="" className='w-full   h-auto hover:cursor-pointer hover:animate-pulse' />
+              </Link>
+          </div>
         {/* header */}
         <LayoutHeader/>
         {/* main */}
         {/* bg-[url('/Imagenes/cuadriculablancagrande.png')] */}
-        <main className="w-full h-full bg-slate-950  backdrop-blur-3xl overflow-hidden" >
+        <main className="  w-full h-full bg-slate-950  backdrop-blur-3xl overflow-hidden" >
+       
         {/* Quienes somos */}
           <HomeQuienesSomos/>
           {/* mission */}
@@ -38,11 +50,13 @@ export const HomeLayout = () => {
           {/* Ubicacion */}
           <HomeUbicacion/>
           {/* footer */}
+          
         </main>
         {/* footer */}
           <HomeFooter/>
           <HomeModales/>
         <ToastContainer/>
+      </div>
     </HomeProviderContext>
   )
 }
