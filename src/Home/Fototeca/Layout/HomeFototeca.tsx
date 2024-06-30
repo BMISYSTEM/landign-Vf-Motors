a7c2c43d5a9348d4828072709d8e6b21233198d1
BMISYTEM <baironmenesidarraga.990128@gmail.com>
2024-06-30 13:45:37 -0500

    el menu lateral paso a menu superior para que las fotos tengan todo el ancho

--- src/Home/Fototeca/Layout/HomeFototeca.tsx
+++ src/Home/Fototeca/Layout/HomeFototeca.tsx
@@ -40,103 +40,105 @@ export const HomeFototeca = () => {
   return (
     <Element name="fototeca">
       <p className="text-2xl font-bold text-slate-300 text-center">Conoce nuestra empresa</p>
-      <section className=" bg-gray-950 shadow-2xl shadow-black w-full h-auto flex md:flex-row flex-col gap-5 p-10 justify-between md:items-start items-center overflow-hidden">
+      <section className=" bg-gray-950 shadow-2xl shadow-black w-full h-auto flex  flex-col gap-5 md:p-10 justify-between md:items-start items-center overflow-hidden">
         {/* opciones */}
         <motion.div
           initial={{ x: -100, opacity: 0 }}
           transition={{ duration: 1.5 }}
           whileInView={{ x: 0, opacity: 1 }}
-          className="w-2/3 flex flex-col gap-4"
+          className="w-full flex flex-col md:gap-4"
         >
-          <div 
-          className={`${opcion.option === 1 ? 'bg-green-500 ' : null} w-full flex h-auto flex-col  bg-[#1D2432] p-4 items-center justify-between`}>
-            <div  onClick={() => {
-                  if (opcion.option === 1) {
-                    setOpcion({ option: 1 });
-                  } else {
-                    setOpcion({ option: 1 });
-                  }
-                  setImagenVisible(1);
-                }} className="w-full flex flex-row gap-2 justify-between cursor-pointer">
-              <p className="md:text-3xl text-lg font-bold text-white flex">
-                Personal
-              </p>
-              <button
-               
-              >
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  viewBox="0 0 24 24"
-                  fill="white"
-                  className="w-6 h-6 font-bold"
-                >
-                  <path
-                    fill-rule="evenodd"
-                    d="M12 3.75a.75.75 0 0 1 .75.75v6.75h6.75a.75.75 0 0 1 0 1.5h-6.75v6.75a.75.75 0 0 1-1.5 0v-6.75H4.5a.75.75 0 0 1 0-1.5h6.75V4.5a.75.75 0 0 1 .75-.75Z"
-                    clip-rule="evenodd"
-                  />
-                </svg>
-              </button>
-            </div>
-          </div>
-          <div className={`${opcion.option === 2 ? 'bg-green-500 ' : null} w-full flex h-auto flex-col  bg-[#1D2432] p-4 items-center justify-between`}>
-            <div onClick={() => {
-                  if (opcion.option === 2) {
-                    setOpcion({ option: 1 });
-                  } else {
-                    setOpcion({ option: 2 });
-                  }
-                  setImagenVisible(1);
-                }} className="w-full flex flex-row gap-2 justify-between cursor-pointer">
-              <p className="md:text-3xl text-lg font-bold text-white flex">
-                Instalaciones
-              </p>
-              <button
+          <div className="w-full flex flex-row md:gap-5 md:justify-center md:items-center overflow-auto ">
+            <div 
+            className={`${opcion.option === 1 ? 'bg-green-500 ' : null} w-full flex h-auto flex-col  bg-[#1D2432] p-4 items-center justify-between`}>
+              <div  onClick={() => {
+                    if (opcion.option === 1) {
+                      setOpcion({ option: 1 });
+                    } else {
+                      setOpcion({ option: 1 });
+                    }
+                    setImagenVisible(1);
+                  }} className="w-full flex flex-row gap-2 justify-between cursor-pointer">
+                <p className="md:text-3xl text-lg font-bold text-white flex">
+                  Personal
+                </p>
+                <button
                 
-              >
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  viewBox="0 0 24 24"
-                  fill="white"
-                  className="w-6 h-6 font-bold"
                 >
-                  <path
-                    fill-rule="evenodd"
-                    d="M12 3.75a.75.75 0 0 1 .75.75v6.75h6.75a.75.75 0 0 1 0 1.5h-6.75v6.75a.75.75 0 0 1-1.5 0v-6.75H4.5a.75.75 0 0 1 0-1.5h6.75V4.5a.75.75 0 0 1 .75-.75Z"
-                    clip-rule="evenodd"
-                  />
-                </svg>
-              </button>
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 24 24"
+                    fill="white"
+                    className="w-6 h-6 font-bold"
+                  >
+                    <path
+                      fill-rule="evenodd"
+                      d="M12 3.75a.75.75 0 0 1 .75.75v6.75h6.75a.75.75 0 0 1 0 1.5h-6.75v6.75a.75.75 0 0 1-1.5 0v-6.75H4.5a.75.75 0 0 1 0-1.5h6.75V4.5a.75.75 0 0 1 .75-.75Z"
+                      clip-rule="evenodd"
+                    />
+                  </svg>
+                </button>
+              </div>
             </div>
-          </div>
-          <div  className={`${opcion.option === 3 ? 'bg-green-500 ' : null} w-full flex h-auto flex-col  bg-[#1D2432] p-4 items-center justify-between`}>
-            <div onClick={() => {
-                  if (opcion.option === 3) {
-                    setOpcion({ option: 1 });
-                  } else {
-                    setOpcion({ option: 3 });
-                  }
-                  setImagenVisible(1);
-                }} className="w-full flex flex-row gap-2 justify-between cursor-pointer">
-              <p className="md:text-3xl text-lg font-bold text-white flex">
-                Entregas
-              </p>
-              <button
-                
-              >
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  viewBox="0 0 24 24"
-                  fill="white"
-                  className="w-6 h-6 font-bold"
+            <div className={`${opcion.option === 2 ? 'bg-green-500 ' : null} w-full flex h-auto flex-col  bg-[#1D2432] p-4 items-center justify-between`}>
+              <div onClick={() => {
+                    if (opcion.option === 2) {
+                      setOpcion({ option: 1 });
+                    } else {
+                      setOpcion({ option: 2 });
+                    }
+                    setImagenVisible(1);
+                  }} className="w-full flex flex-row gap-2 justify-between cursor-pointer">
+                <p className="md:text-3xl text-lg font-bold text-white flex">
+                  Instalaciones
+                </p>
+                <button
+                  
                 >
-                  <path
-                    fill-rule="evenodd"
-                    d="M12 3.75a.75.75 0 0 1 .75.75v6.75h6.75a.75.75 0 0 1 0 1.5h-6.75v6.75a.75.75 0 0 1-1.5 0v-6.75H4.5a.75.75 0 0 1 0-1.5h6.75V4.5a.75.75 0 0 1 .75-.75Z"
-                    clip-rule="evenodd"
-                  />
-                </svg>
-              </button>
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 24 24"
+                    fill="white"
+                    className="w-6 h-6 font-bold"
+                  >
+                    <path
+                      fill-rule="evenodd"
+                      d="M12 3.75a.75.75 0 0 1 .75.75v6.75h6.75a.75.75 0 0 1 0 1.5h-6.75v6.75a.75.75 0 0 1-1.5 0v-6.75H4.5a.75.75 0 0 1 0-1.5h6.75V4.5a.75.75 0 0 1 .75-.75Z"
+                      clip-rule="evenodd"
+                    />
+                  </svg>
+                </button>
+              </div>
+            </div>
+            <div  className={`${opcion.option === 3 ? 'bg-green-500 ' : null} w-full flex h-auto flex-col  bg-[#1D2432] p-4 items-center justify-between`}>
+              <div onClick={() => {
+                    if (opcion.option === 3) {
+                      setOpcion({ option: 1 });
+                    } else {
+                      setOpcion({ option: 3 });
+                    }
+                    setImagenVisible(1);
+                  }} className="w-full flex flex-row gap-2 justify-between cursor-pointer">
+                <p className="md:text-3xl text-lg font-bold text-white flex">
+                  Entregas
+                </p>
+                <button
+                  
+                >
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 24 24"
+                    fill="white"
+                    className="w-6 h-6 font-bold"
+                  >
+                    <path
+                      fill-rule="evenodd"
+                      d="M12 3.75a.75.75 0 0 1 .75.75v6.75h6.75a.75.75 0 0 1 0 1.5h-6.75v6.75a.75.75 0 0 1-1.5 0v-6.75H4.5a.75.75 0 0 1 0-1.5h6.75V4.5a.75.75 0 0 1 .75-.75Z"
+                      clip-rule="evenodd"
+                    />
+                  </svg>
+                </button>
+              </div>
             </div>
           </div>
         </motion.div>
@@ -145,9 +147,9 @@ export const HomeFototeca = () => {
           initial={{ x: 100, opacity: 0 }}
           transition={{ duration: 1 }}
           whileInView={{ x: 0, opacity: 1 }}
-          className="w-full h-full flex flex-col rounded-xl gap-5 items-center justify-center"
+          className="w-full  h-full flex flex-col rounded-xl gap-5 items-center justify-center"
         >
-          <div className="w-full h-96 flex flex-col">
+          <div className="w-full h-[35rem] flex flex-col">
             <p className="text-xl font-bold text-slate-200 text-center">
               {opcion.option === 1
                 ? "Personal"
