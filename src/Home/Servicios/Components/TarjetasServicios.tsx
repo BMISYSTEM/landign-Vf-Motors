9e62c8733df22fa20bb81134baa5a2422ad38c63
BMISYTEM <baironmenesidarraga.990128@gmail.com>
2024-06-30 13:52:00 -0500

    se transformo en un enlace a wpp

--- src/Home/Servicios/Components/TarjetasServicios.tsx
+++ src/Home/Servicios/Components/TarjetasServicios.tsx
@@ -4,13 +4,13 @@ interface props {
 }
 export const TarjetasServicios = ({ label, imagen }: props) => {
   return (
-    <div className="border-2 mt-0 bg-[#1D2432] shadow-lg hover:shadow-green-500 border-green-500 rounded-sm hover:scale-110 transition-all hover:border-white md:w-52 md:h-60 w-20 h-24 flex flex-col gap-2 justify-center items-center">
+    <a href="https://wa.me/message/IH5SZDTONQ5MK1" className="border-2 mt-0 bg-[#1D2432] shadow-lg hover:shadow-green-500 border-green-500 rounded-sm hover:scale-110 transition-all hover:border-white md:w-36 md:h-48 w-20 h-auto flex flex-col gap-2 justify-center items-center">
       {/* imagen  */}
-      <img src={imagen} alt="imagen de servicio" />
+      <img src={imagen} alt="imagen de servicio" className="w-16 h-auto" />
       {/* titulo */}
-      <p className="md:text-lg text-sm font-bold text-white text-justify">
+      <p className="md:text-lg text-sm font-bold text-white text-center">
         {label}
       </p>
-    </div>
+    </a>
   );
 };
